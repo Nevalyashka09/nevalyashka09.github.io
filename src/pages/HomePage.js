1b120b175b091af262075a85c6973e24f8d2c6bb
OLESIA TUR <olesia.tur@gmail.com>
2024-08-26 16:03:03 +0200

    home page - section three - timeline styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -29,25 +29,21 @@ const educationData = [
     date: "Feb - Jul 2023",
     position: "Fullstack Web Developer",
     company: "Wild Code School, Bordeaux",
-    arrow: arrowLeft,
   },
   {
     date: "Mar - Apr 2021",
     position: "Piscine",
     company: "Ecole 42, Paris",
-    arrow: arrowLeft,
   },
   {
     date: "Sep 2011 - Jun 2015",
     position: "Master's Degree",
     company: "Fudan University, Shanghai",
-    arrow: arrowLeft,
   },
   {
     date: "Sep 2007 - Jun 2011",
     position: "Bachelor's Degree",
     company: "Shevchenko University, Kyiv",
-    arrow: arrowLeft,
   },
 ];
 
@@ -55,51 +51,18 @@ const experienceData = [
   {
     date: "Mar 2024",
     position: "Freelance Web Developer",
-    arrow: arrowRight,
-    reverse: true,
   },
   {
     date: "Sep 2023 - Mar 2024",
     position: "Front End Developer",
     company: "Qomon, Bordeaux",
-    arrow: arrowRight,
   },
   {
     date: "May 2021 - Dec 2022",
     position: "Team Manager",
     company: "Gucci, Paris",
-    arrow: arrowRight,
   },
 ];
-
-const TimelineEntry = ({
-  date,
-  position,
-  company,
-  arrow = false,
-  arrowPosition = "after",
-}) => (
-  <div className="timeline">
-    {arrow && arrowPosition === "before" && (
-      <div>
-        <img src={arrow} alt="Arrow" className="arrow" />
-      </div>
-    )}
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <p className="p-istok" style={{ fontSize: "1rem" }}>
-        {company}
-      </p>
-      <p className="p-istok" style={{ fontSize: "1rem" }}>
-        <span style={{ color: "#1b5536" }}>{position}</span> | {date}
-      </p>
-    </div>
-    {arrow && arrowPosition === "after" && (
-      <div>
-        <img src={arrow} alt="Arrow" className="arrow" />
-      </div>
-    )}
-  </div>
-);
 
 const TechCard = ({ name, src }) => (
   <div className="techno-card">
@@ -114,6 +77,49 @@ const TechCard = ({ name, src }) => (
     >
       {name}
     </p>
+  </div>
+);
+
+const EducationTimeLineItem = ({ company, position, date }) => (
+  <div className="timeline" style={{ justifyContent: "flex-end" }}>
+    <div style={{ flexDirection: "column", display: "flex" }}>
+      <p className="p-istok" style={{ fontSize: "1rem" }}>
+        {company}
+      </p>
+      <p className="p-istok" style={{ fontSize: "1rem" }}>
+        <span style={{ color: "#1b5536", fontFamily: "istokWebBold" }}>
+          {position}
+        </span>{" "}
+        | {date}
+      </p>
+    </div>
+    <div>
+      <img src={arrowLeft} alt="Arrow" className="arrow" />
+    </div>
+  </div>
+);
+
+const ExperienceTimeLineItem = ({ company, position, date }) => (
+  <div class="timeline">
+    <div>
+      <img src={arrowRight} alt="Arrow" className="arrow" />
+    </div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <p className="p-istok" style={{ fontSize: "1rem" }}>
+        {company}
+      </p>
+      <p className="p-istok" style={{ fontSize: "1rem" }}>
+        <span style={{ color: "#1b5536", fontFamily: "istokWebBold" }}>
+          {position}
+        </span>{" "}
+        | {date}
+      </p>
+    </div>
   </div>
 );
 
@@ -175,31 +181,60 @@ const HomePage = () => {
       </section>
       <section className="background">
         <div className="education">
-          <div className="timeline-header">
+          <div
+            class="timeline"
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <h1>My Background</h1>
           </div>
-          {educationData.map((entry, index) => (
-            <TimelineEntry
-              key={index}
-              date={entry.date}
-              position={entry.position}
-              company={entry.company}
-              arrow={entry.arrow}
-              arrowPosition="after"
-            />
-          ))}
+          <div class="timeline"></div>
+          <div class="timeline"></div>
+          <EducationTimeLineItem
+            company={educationData[0].company}
+            position={educationData[0].position}
+            date={educationData[0].date}
+          />
+          <EducationTimeLineItem
+            company={educationData[1].company}
+            position={educationData[1].position}
+            date={educationData[1].date}
+          />
+          <div class="timeline"></div>
+          <EducationTimeLineItem
+            company={educationData[2].company}
+            position={educationData[2].position}
+            date={educationData[2].date}
+          />
+          <EducationTimeLineItem
+            company={educationData[3].company}
+            position={educationData[3].position}
+            date={educationData[3].date}
+          />
         </div>
         <div className="experience">
-          {experienceData.map((entry, index) => (
-            <TimelineEntry
-              key={index}
-              date={entry.date}
-              position={entry.position}
-              company={entry.company}
-              arrow={entry.arrow}
-              arrowPosition="before"
-            />
-          ))}
+          <div class="timeline"></div>
+          <ExperienceTimeLineItem
+            position={experienceData[0].position}
+            date={experienceData[0].date}
+          />
+          <ExperienceTimeLineItem
+            company={experienceData[1].company}
+            position={experienceData[1].position}
+            date={experienceData[1].date}
+          />
+          <div class="timeline"></div>
+          <div class="timeline"></div>
+          <ExperienceTimeLineItem
+            company={experienceData[2].company}
+            position={experienceData[2].position}
+            date={experienceData[2].date}
+          />
+          <div class="timeline"></div>
+          <div class="timeline"></div>
         </div>
       </section>
     </section>
