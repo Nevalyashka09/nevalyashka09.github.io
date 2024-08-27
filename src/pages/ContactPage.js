cb2824c377c067dbfd8dd86d47497915fd83ac08
OLESIA TUR <olesia.tur@gmail.com>
2024-08-27 15:26:22 +0200

    contact page - Part 1

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,11 +1,46 @@
 import React from "react";
 import "../styles/contactPage.scss";
+import paintBackground from "../images/paintBackground2.png";
 
 const ContactPage = () => {
   return (
-    <div className="contactPage">
-      <h1>Contact Page</h1>
-      <p>This is the contact page.</p>
+    <div>
+      <div className="light-blue-rectangle" />
+      <div className="header-container">
+        <h1 style={{ fontFamily: "homemadeApple", position: "relative" }}>
+          Let's get in touch <span style={{ color: "#ff4d00" }}>...</span>
+        </h1>
+      </div>
+      <div className="contact-form">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <div className="contact-text-container">
+            <p className="p-istok" style={{ fontSize: "1.1rem" }}>
+              Have any questions or just{" "}
+              <span style={{ color: "#ff4d00" }}>want to say hi</span>?
+            </p>
+            <p className="p-istok" style={{ fontSize: "1.1rem" }}>
+              <span style={{ color: "#ff4d00" }}>
+                I’d love to hear from you!{" "}
+              </span>
+              Whether you have inquiries about my work, project ideas, or just
+              want to connect, feel free to send me a message. Looking forward
+              to chatting with you!
+            </p>
+          </div>
+          <img
+            src={paintBackground}
+            alt="Paint Background"
+            className="contact-paint-background"
+          />
+        </div>
+        <div className="form-container"></div>
+      </div>
     </div>
   );
 };
