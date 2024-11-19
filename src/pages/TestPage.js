f4cb65a4f48f93315684a2d2d535a5ef47f77233
OLESIA TUR <olesia.tur@gmail.com>
2024-11-19 16:15:02 +0100

    switch toggle

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,8 +1,8 @@
 import React from "react";
-import SocialButtons from "../portfolioComponents/sampleComponents/SocialButtons";
+import SwitchToggle from "../portfolioComponents/sampleComponents/SwitchToggle";
 
 const TestPage = () => {
-  return <SocialButtons />;
+  return <SwitchToggle />;
 };
 
 export default TestPage;
